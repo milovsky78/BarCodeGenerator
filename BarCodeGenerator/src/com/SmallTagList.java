962615446fa6d1f977133e37d34e911dca97a34e
Home <Home@Milo-Note>
2017-07-07 10:28:25 +0800

    Revert "Revert """
    This reverts commit a3f88f9db0ba91b42621ac9d008fdd80c820dfa7.

---
+++ BarCodeGenerator/src/com/SmallTagList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+import java.awt.*;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author Home
+ */
+public class SmallTagList extends JPanel{
+  
+  public SmallTagList(){
+
+  }
+  
+  protected void paintComponent(Graphics g,String[] string){
+
+    
+  }
+  
+}
